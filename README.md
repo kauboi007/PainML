f2ac400d270a765cf25e942318219fcecdbd4b46
Kaushik M <kaushikmugundan2019@gmail.com>
2026-06-29 18:45:46 +0530

    bias added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 training my one neural ML model 
 aka training my last braincell
+
+GPT 4 has 1 000 000 000 000 parameters
+this clanker 2 parameters
+
+very close
+open ai is shaking rn
--- main.c
+++ main.c
@@ -15,11 +15,11 @@ float rand_float(){
     return (float)rand()/(float) RAND_MAX;
 }
 
-float cost(float w){
+float cost(float w,float b){
     float res=0.0;
     for(size_t i=0;i<train_count;++i){
         float x=train[i][0];
-        float y=x*w;
+        float y=x*w+b;
         float d=y-train[i][1];
         res +=d*d;
         //printf("actual : %f vs expected : %f\n",y,train[i][1]);
@@ -32,12 +32,15 @@ int main(){
     //srand(time(0));
     srand(69);
     float w=rand_float()*10;
+    float b=rand_float()*5;
     float eps=1e-3;
     float rate=1e-3;
-    printf("initial:%f\n",cost(w));
+    printf("initial:%f\n",cost(w,b));
     for(int i=0;i<1500;++i){
-        float dcost=(cost(w+eps)-cost(w))/eps;
-        w-=rate*dcost;
+        float dw=(cost(w+eps,b)-cost(w,b))/eps;
+        float db=(cost(w,b+eps)-cost(w,b))/eps;
+        w-=rate*dw;
+        b-=rate*db;
         //printf("iteration %d : %f\n",i+1,cost(w));
     }
     printf("final:%f\n",w);
